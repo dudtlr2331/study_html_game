050c015660008a9249822f1969724d7399791430
dudtlr2331 <dudtlr102@naver.com>
2021-05-01 12:00:00 +0900

    05/01 2번째

--- step01/DodgeGame.js
+++ step01/DodgeGame.js
@@ -4,16 +4,6 @@ window.addEventListener("keyup", onKeyup, false);
 
 var intPlayerX = 350;
 var intPlayerY = 250;
-var imgMissileX = 0;
-var imgMissileY = 0;
-
-var imgMissileX2 = 740;
-var imgMissileY2 = 0;
-var imgMissileX3 = 0;
-var imgMissileY3 = 540;
-var imgMissileX4 = 740;
-var imgMissileY4 = 540;
-
 var intPlayerSpeed = 50;
 
 var GAME_START_READY = 0;
@@ -22,31 +12,18 @@ var GAME_START_OVER = 2;
 
 var gameState = GAME_START_READY;
 
+var arrMissiles = new Array();
 
 var imgBackground = new Image();
 imgBackground.src = "../images/background.png";
-imgBackground.addEventListener("load", drawScreen, false);
 
 var imgPlayer = new Image();
 imgPlayer.src = "../images/player.png"
-imgPlayer.addEventListener("load", drawScreen, false);
 
 var imgMissile = new Image();
-imgMissile.src = "../images/missile.png"
-imgMissile.addEventListener("load", drawScreen, false);
+imgMissile.src = "../images/missile.png";
 
-var imgMissile2 = new Image();
-imgMissile2.src = "../images/missile.png"
-imgMissile2.addEventListener("load", drawScreen, false);
-
-var imgMissile3 = new Image();
-imgMissile3.src = "../images/missile.png"
-imgMissile3.addEventListener("load", drawScreen, false);
-
-var imgMissile4 = new Image();
-imgMissile4.src = "../images/missile.png"
-imgMissile4.addEventListener("load", drawScreen, false);
-
+//수치가 계산된 내용을 그려주는 역활
 function drawScreen(){
     var theCanvas =  document.getElementById("GameCanvas");
     var Context = theCanvas.getContext("2d");
@@ -56,46 +33,79 @@ function drawScreen(){
 
     Context.drawImage(imgBackground, 0, 0);
     Context.drawImage(imgPlayer, intPlayerX, intPlayerY);
-    Context.drawImage(imgMissile, imgMissileX, imgMissileY);
-    Context.drawImage(imgMissile2, imgMissileX2, imgMissileY2);
-    Context.drawImage(imgMissile3, imgMissileX3, imgMissileY3);
-    Context.drawImage(imgMissile4, imgMissileX4, imgMissileY4);
 
-    Context.fillStyle = "#ffffff";
+    Context.fillStyle = "#FFFFFF";
     Context.font = "50px Arial";
     Context.textBaseline = "top";
-    
+
     if(gameState == GAME_START_READY){    
         Context.fillText("준비", 330, 180);
     }
-    if(gameState == GAME_START_GAME){
 
+    if(gameState == GAME_START_GAME){
+        for(var i=0; i<arrMissiles.length; i++){
+            Context.drawImage(imgMissile, arrMissiles[i].x, arrMissiles[i].y);
+        }
     }
+
     if(gameState == GAME_START_OVER){
         Context.fillText("게임오버", 300, 180);
     }
 }
 
+//수치값 계산
+var intTime = 0;
 function InGameUpdate(){
-    imgMissileX +=intPlayerSpeed;
-    imgMissileY +=intPlayerSpeed; 
-    imgMissileX2 -=intPlayerSpeed;
-    imgMissileY2 +=intPlayerSpeed;
-    imgMissileX3 +=intPlayerSpeed;
-    imgMissileY3 -=intPlayerSpeed;
-    imgMissileX4 -=intPlayerSpeed;
-    imgMissileY4 -=intPlayerSpeed;
+    var intX=0, intY=0, intGoX=0, intGoY=0;
+    if(intTime % 5000 == 0){
+        for(var i=0; i<4; i++){
+            switch(i){
+                case 0: //위,왼쪽
+                intX = 0;
+                intY = 0;
+                intGoX = 5;
+                intGoY = 5;
+                break;
+                case 1: //위, 오른쪽
+                intX = 740;
+                intY = 0;
+                intGoX = -5;
+                intGoY = 5;
+                break;
+                case 2: //아래, 왼쪽
+                intX = 0;
+                intY = 540;
+                intGoX = 5;
+                intGoY = -5;
+                break;
+                case 3: //아래, 오른쪽
+                intX = 740;
+                intY = 540;
+                intGoX = -5;
+                intGoY = -5;
+                break;
+            }
+            arrMissiles.push({x:intX, y:intY, go_x:intGoX, go_y:intGoY});
+        }
+    }
+    intTime+=100;
+    MoveMissile();
+}
+function MoveMissile(){
+    //움직임을 봐쭤주는 로직 작성..
+    for(var i=0; i<arrMissiles.length; i++){
+       arrMissiles[i].x += arrMissiles[i].go_x;
+       arrMissiles[i].y += arrMissiles[i].go_y;
+    }
     drawScreen();
 }
-
 function onKeydown(e){
     if(gameState == GAME_START_READY){    
         if(e.keyCode == 13){
             gameState = GAME_START_GAME;
-            setInterval(InGameUpdate, 100)
+            setInterval(InGameUpdate, 100);
         }
     }
-
     if(gameState == GAME_START_GAME){
         switch(e.keyCode){
             case 37: //왼쪽
@@ -130,8 +140,6 @@ function onKeydown(e){
     }
     drawScreen();
 }
-
 function onKeyup(e){
-
     drawScreen();
 }
